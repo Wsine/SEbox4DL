5148b88884889c97e64e23f7f8fc3faa60e68c48
Jerrymouse <344592457@qq.com>
2021-11-21 15:59:00 +0800

    add new task mutate (#3)
    * add new task mutate
    
    * add dowonload button to mutate
    
    * add dowonload button to mutate

--- app/tasks/mutate.py
+++ app/tasks/mutate.py
@@ -1,5 +1,69 @@
+import random
+from argparse import Namespace
+import torch
 import streamlit as st
+from app import sidebar
+from app.context import st_stdout, st_stderr
+from src.model import load_model
+from src.dataset import load_dataset
+from src.mutate import absent_mutate
+import numpy as np
+import matplotlib.pyplot as plt
 
-def run():
-    st.write('mutate')
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
+
+def load_sidebar(ctx):
+    opt = Namespace()
+    sidebar.load_datasets(opt)
+    sidebar.load_models(opt)
+    return opt
+
+
+def run(ctx):
+    st.write('# Task: Mutate')
+    st.write('## Configs')
+    st.json(vars(ctx.opt))
+
+    st.write('## Logs')
+
+    ctx.device = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')
+
+    with st.spinner(text='Loading model...'), st.expander('See loading process'):
+        with st_stdout('code'), st_stderr('code'):
+            model = load_model(ctx.opt).to(ctx.device)
+    st.success(':balloon: model loaded.')
+
+    with st.spinner(text='Loading dataset...'), st.expander('See loading process'):
+        with st_stdout('code'), st_stderr('code'):
+            _, testloader = load_dataset(ctx.opt, split='test')
+    st.success(':balloon: dataset loaded.')
+    buggy_filters, buggy_filters_layer, mutated_models = absent_mutate(ctx=ctx, model=model, data_loader=testloader)
+    sorted_acc_diff, sorted_indices = torch.sort(torch.tensor(buggy_filters['acc_diff']), descending=True)
+
+    st.write('## Results')
+    st.write('### 1. Buggy filters test accuracy difference')
+    acc_diff = sorted_acc_diff.tolist()
+    plt.title('Test accuracy difference with filter absent')
+    plt.plot(range(len(acc_diff)), acc_diff)
+    plt.xlabel('Filter Index')
+    plt.ylabel('Test Accuracy Difference')
+    st.pyplot()
+
+    st.write('### 2. Download mutated models')
+    try:
+        model_index_list = random.sample(range(0, len(mutated_models)), 2)
+        for model_index in model_index_list:
+            torch.save(mutated_models[model_index], "mutated_model_randomly_" + str(model_index) + ".pth")
+            st.download_button(label="Download mutated model_" + str(model_index),
+                               data="mutated_model_randomly_" + str(model_index) + ".pth",
+                               file_name="mutated_model_randomly_" + str(model_index) + ".pth")
+    except Exception as excep:
+        st.write('No mutated models can be downloaded! ' + str(excep))
+
+    st.write('### 3. Buggy filters list')
+    for buggy_filters_one_layer in buggy_filters_layer:
+        if len(buggy_filters_one_layer['buggy_filter_index_layer']) > 0:
+            st.write("Layer: " + buggy_filters_one_layer['layer'].strip('.weight') + " buggy filter index: " + str(buggy_filters_one_layer['buggy_filter_index_layer']))
+
+    st.balloons()
---
+++ src/mutate.py
@@ -0,0 +1,64 @@
+import torch
+import copy
+from src.eval import eval_accuracy
+
+
+def absent_mutate(ctx, model, data_loader):
+    '''
+    buggy filters means that after we delete the filter, the valid accuracy will increase somehow.
+    So this is where a key distinction between terms comes in handy:
+    whereas in the 1 channel case, where the term filter and kernel are interchangeable. In the general case,
+    they’re actually pretty different. Each filter actually happens to be a collection of kernels, with there
+    being one kernel for every single input channel to the layer, and each kernel being unique.
+
+    :param ctx: context
+    :param model: trained model
+    :param data_loader: validation or test loader
+    :return: find the buggy filters for dataset level
+    '''
+    buggy_filters = {'acc_diff': [], 'buggy_filter_index': []}
+    buggy_filters_by_layer = []
+
+    std_acc = eval_accuracy(ctx=ctx, model=model, testloader=data_loader, desc='std_acc')
+    print(f"Standard accuracy is: {std_acc}")
+
+    state_dict_original = copy.deepcopy(model.state_dict())
+    layer_list = list(state_dict_original)
+
+    mutated_models = []
+
+    for layer_index, layer in enumerate(state_dict_original):
+        layer_size = state_dict_original[layer].size()
+        acc_diff_layer = []
+        buggy_filter_index_layer = []
+
+        keep_model_layer = False
+        # Explore and find step: finding the buggy filters
+        if len(layer_size) == 4:
+            # print(f"----Exploration: Current Layer is {layer}, Size is {state_dict_original[layer].size()}----")
+            for filter_index in range(layer_size[0]):
+                state_dict_modified = copy.deepcopy(state_dict_original)
+                state_dict_modified[layer][filter_index] = state_dict_modified[layer][filter_index] * 0
+
+                # also set the corresponding bias to zero
+                if str(layer_list[layer_index+1]).endswith('bias'):
+                    state_dict_modified[layer_list[layer_index + 1]][filter_index] = 0
+
+                model.load_state_dict(state_dict_modified)
+                mutate_acc = eval_accuracy(ctx=ctx, model=model, testloader=data_loader, desc='std_acc')
+
+                acc_diff = torch.tensor(mutate_acc) - torch.tensor(std_acc)
+                acc_diff_layer.append(acc_diff.tolist())
+                if acc_diff > 0:
+                    buggy_filter_index_layer.append(filter_index)
+
+                    # one layer only keep one mutated model
+                    if keep_model_layer is False:
+                        mutated_models.append(copy.deepcopy(model.state_dict()))
+                        keep_model_layer = True
+
+            # keep the buggy filter info
+            buggy_filters['acc_diff'].extend(acc_diff_layer)
+            buggy_filters['buggy_filter_index'].extend(buggy_filter_index_layer)
+            buggy_filters_by_layer.append({'layer': layer, 'acc_diff_layer': acc_diff_layer, 'buggy_filter_index_layer': buggy_filter_index_layer})
+    return buggy_filters, buggy_filters_by_layer, mutated_models
