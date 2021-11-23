617bd34501f9f9bdcca20898b42b795d67055e47
Wsine <weizhy3@hotmail.com>
2021-11-23 12:01:01 +0800

    refactor train task

--- app/sidebar.py
+++ app/sidebar.py
@@ -42,44 +42,34 @@ def load_models(opt):
     elif opt.model_source_type == '3rdparty':
         opt.model_source = st.text_input(
             'where is the model source?',
-             # value="chenyaofo/pytorch-cifar-models",
             placeholder='example: chenyaofo/pytorch-cifar-models',
             help='refer to: https://pytorch.org/hub/'
         )
         if len(opt.model_source) > 0:
             models = torch.hub.list(opt.model_source)
             opt.model = st.selectbox('which model to be tested?', models)
-            #  opt.model = st.selectbox('which model to be tested?', models, index=32)
     elif opt.model_source_type == 'local':
         # TODO: scan and add local models
         pass
     else:
         raise ValueError('Invalid input source')
 
-@sidebar_ctx
-def load_evaluate_configs(opt):
-    opt.pretrained = st.checkbox('use (pre)trained weights?', True)
 
 @sidebar_ctx
-def load_train_configs(opt, isfinetune=False):
-    opt.max_epoch = st.number_input('What is the maximum training epoch', min_value=1, value=5)
-    opt.optimizer = st.radio(
-        'which optimizer to be used?',
-        ['SGD', 'Adam', 'Adagrad']
-    )
-
-    opt.loss = st.radio(
-        'which loss function to be used?',
-        ['Cross-entropy', 'Mean squared error']
-    )
-    if isfinetune:
-        opt.pretrained = True
-    else:
-        opt.pretrained = False
-    opt.output_dir = "./output/"
+def load_pretrained(opt, preset=True):
+    opt.pretrained = st.checkbox('use (pre)trained weights?', preset)
 
 
+@sidebar_ctx
+def load_train_options(opt):
+    optimizers = [o for o in torch.optim.__dict__.keys() if o[0].isupper()]
+    opt.optimizer = st.selectbox('which optimizer to be used?', optimizers, index=optimizers.index('SGD'))
 
+    criteria = [l for l in torch.nn.__dict__.keys() if l.endswith('Loss')]
+    opt.criterion = st.selectbox('which loss function to be used?', criteria, index=criteria.index('CrossEntropyLoss'))
 
-
+    opt.max_epoch = st.number_input('What is the maximum training epoch', min_value=1, value=20)
+    opt.lr = st.number_input('What is the learning rate', value=0.01, format='%.4f')
+    opt.momentum = st.number_input('What is the momentum', value=0.9, format='%.4f')
+    opt.weight_decay = st.number_input('What is the weight decay', value=5e-4, format='%.4f')
 
